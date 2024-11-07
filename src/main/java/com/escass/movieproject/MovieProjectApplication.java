7aa1f2b72401a674726f01630bf2dafda8e0d391
escurse <av1967@naver.com>
2024-11-07 11:14:18 +0900

    movie

--- src/main/java/com/escass/movieproject/MovieProjectApplication.java
+++ src/main/java/com/escass/movieproject/MovieProjectApplication.java
@@ -15,10 +15,6 @@ public class MovieProjectApplication {
 
         String URL = "http://www.cgv.co.kr/movies/detail-view/still-cut.aspx?midx=88459#menu";
         Document doc = Jsoup.connect(URL).get();
-        System.out.println(doc);
-        Elements elements = doc.getElementsByTag("div");
-        for (Element element : elements) {
-            Elements elements1 = doc.getElementsByTag("a");
-        }
+        System.out.println(doc.text());
     }
 }
