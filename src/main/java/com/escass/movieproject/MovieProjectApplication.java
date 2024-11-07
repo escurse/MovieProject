20a97fcfc1e8a10f4ccf9854ca5b6e212aa35791
escurse <av1967@naver.com>
2024-11-07 15:34:45 +0900

    movie

--- src/main/java/com/escass/movieproject/MovieProjectApplication.java
+++ src/main/java/com/escass/movieproject/MovieProjectApplication.java
@@ -2,7 +2,6 @@ package com.escass.movieproject;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +12,10 @@ public class MovieProjectApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MovieProjectApplication.class, args);
 
-        String URL = "http://www.cgv.co.kr/movies/detail-view/still-cut.aspx?midx=88459#menu";
+        String URL = "http://www.cgv.co.kr/theaters/?areacode=11&theaterCode=0157&date=20241107#tile_0";
         Document doc = Jsoup.connect(URL).get();
-        System.out.println(doc.text());
+        System.out.println(doc);
+        Elements element = doc.select(".thumb-image");
+        System.out.println(element);
     }
 }
